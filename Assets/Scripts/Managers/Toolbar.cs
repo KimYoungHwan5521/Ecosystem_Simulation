42cf549612d0bb4932193dfd4bf5c63a87c97818
KIM YOUNG HWAN <aorkslrdnjwm@naver.com>
2024-12-17 13:08:20 +0900

    Camera works

--- Assets/Scripts/Managers/Toolbar.cs
+++ Assets/Scripts/Managers/Toolbar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class Toolbar : MonoBehaviour
@@ -9,5 +10,22 @@ public class Toolbar : MonoBehaviour
 
     public Toggle showNutrient;
 
+    Vector2 navVector;
+
+    private void Update()
+    {
+        Camera.main.transform.position += (Vector3)navVector * Camera.main.orthographicSize * 0.2f;
+
+    }
     public void ToggleShowNutrient() { bShowNutrient = !bShowNutrient; }
+
+    void OnNavigate(InputValue value)
+    {
+        navVector = value.Get<Vector2>();
+    }
+
+    void OnScrollWheel(InputValue value)
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - value.Get<Vector2>().y * 0.01f, 1, 10);
+    }
 }
